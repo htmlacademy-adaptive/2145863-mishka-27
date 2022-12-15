5d75aac2c0cfb6d60c953509e3a5cf85ad4491b2
Alexander Fedorov <ru9.alex@gmail.com>
2022-12-15 14:01:18 +0300

    Добавляет яндекс карту

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -9,3 +9,36 @@ function onMenuTogglerClick() {
 
 menuToggle.addEventListener('click', onMenuTogglerClick);
 
+
+  // Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        center: [59.938735, 30.322958],
+        // от 0 (весь мир) до 19.
+        zoom: 17
+    });
+
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        // hintContent: 'Собственный значок метки',
+        // balloonContent: 'Это красивая метка'
+    }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/icons/map-pin.svg',
+        // Размеры метки.
+        iconImageSize: [66, 99],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-22, -82]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
+
+ymaps.ready(init);
