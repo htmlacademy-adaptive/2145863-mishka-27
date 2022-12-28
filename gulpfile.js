b15ed09727ce30ad8116d13214926df58d6b032e
Alexander Fedorov <ru9.alex@gmail.com>
2022-12-28 19:03:12 +0300

    Добаляет сборку

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,113 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import htmlmin from 'gulp-htmlmin';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
+import terser from 'gulp-terser';
+// пакет gulp-libsquoosh заменен на gulp-squoosh (проблема с avif на windows, давно не обновляется)
+// https://github.com/GoogleChromeLabs/squoosh/issues/1119
+// import squoosh from 'gulp-libsquoosh'; 
+import squoosh from 'gulp-squoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
 
 export const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
+    // не дает js упасть при ошибке в стилях (нет нужды запускать каждый раз npm start)
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))    
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browser.stream())
+}
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh({
+      encodeOptions: {
+        oxipng: {},
+        mozjpeg: {},
+        webp: {},
+        avif: {},
+      }
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// Copy images
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+
+const svg = () => {
+  return gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+// SVG Sprite
+
+const sprite = () => {
+  return gulp.src('source/img/sprite-icons/**/*.svg')
+    .pipe(svgo())  
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'))
+}
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/*.{woff2,woff}',
+    'source/*.ico',
+    'source/manifest.webmanifest'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+// Clean build
+
+const clean = () => {
+  return del('build')
 }
 
 // Server
@@ -23,7 +116,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -32,14 +125,48 @@ const server = (done) => {
   done();
 }
 
+// Reload
+
+const reload = (done) => {
+  browser.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/js/**/*.js', gulp.series(scripts));
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
+export const build = gulp.series(
+  clean,
+  gulp.parallel(
+    copy,
+    optimizeImages,
+    styles,
+    html,
+    scripts,
+    svg,
+    sprite
+  )
+);
 
 export default gulp.series(
-  styles, server, watcher
+  clean,
+  gulp.parallel(
+    copy,
+    // оптимизация и создание webp и avif реализовано в build (операция занимает около минуты)
+    copyImages,
+    styles,
+    html,
+    scripts,
+    svg,
+    sprite
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
